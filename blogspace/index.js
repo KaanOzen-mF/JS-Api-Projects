38334a3dece6d9638209c723214ac9a56d5b586f
KaanOzen-mF <kaanozn98@gmail.com>
2023-08-10 11:39:33 +0300

    PUSH method implemented

--- blogspace/index.js
+++ blogspace/index.js
@@ -1,18 +1,25 @@
+let postArray = [];
+
+function renderPost() {
+  let html = "";
+  for (let post of postArray) {
+    html += `
+        <h3>${post.title}</h3>
+        <p>${post.body}</p>
+        <hr />
+    `;
+  }
+  document.getElementById("post-container").innerHTML = html;
+}
+
 fetch("https://apis.scrimba.com/jsonplaceholder/posts")
   .then((res) => res.json())
   .then((res) => {
-    const posts = res.slice(1, 5);
-    for (let i = 0; i < posts.length; i++) {
-      document.getElementById(
-        "post-container"
-      ).innerHTML += `<h2>${posts[i].title}</h2>
-                      <p>${posts[i].body}</p>
-                      <hr>
-                      `;
-    }
+    postArray = res.slice(1, 5);
+    renderPost();
   });
 
-document.getElementById("new-post").addEventListener("sumbit", (e) => {
+document.getElementById("new-post").addEventListener("submit", (e) => {
   e.preventDefault();
   const postTitle = document.getElementById("post-title").value;
   const postBody = document.getElementById("post-body").value;
@@ -20,5 +27,19 @@ document.getElementById("new-post").addEventListener("sumbit", (e) => {
     title: postTitle,
     body: postBody,
   };
-  console.log(data);
+
+  const options = {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+    .then((res) => res.json())
+    .then((post) => {
+      postArray.unshift(post);
+      renderPost();
+    });
 });
