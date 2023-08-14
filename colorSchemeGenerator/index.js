1aba19666cb5d3619cc990486134bb99a0732b72
KaanOzen-mF <kaanozn98@gmail.com>
2023-08-14 10:25:45 +0300

    All design files updated

---
+++ colorSchemeGenerator/index.js
@@ -0,0 +1,4 @@
+document.getElementById("color").addEventListener("change", function () {
+  var selectedColor = this.value;
+  this.style.backgroundColor = selectedColor; // Set the background color of the select element
+});
