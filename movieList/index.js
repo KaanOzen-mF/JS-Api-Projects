fe08e9cfb8bf376fa4c2661eaede7c563c40afa4
KaanOzen-mF <kaanozn98@gmail.com>
2023-08-24 09:13:16 +0300

    displayMovies function updated

--- movieList/index.js
+++ movieList/index.js
@@ -2,14 +2,16 @@
 const movieSearchInput = document.getElementById("movie-search");
 const searchButton = document.getElementById("search-button");
 const iconContainer = document.getElementById("icon-container");
+const movieContainer = document.getElementById("main-section");
 
 //Fetch API function, it needs movie name as parameter
 const fetchApiData = async (movieName) => {
   //Try fetch API
   try {
     const res = await fetch(
-      `http://www.omdbapi.com/?apikey=cf0fdf8&s=${movieName}&type=movie`
+      `https://www.omdbapi.com/?apikey=cf0fdf8&s=${movieName}&type=movie`
     );
+
     //If response is not okey, it throw fail error
     if (!res.ok) {
       throw new Error("Failed to fetch data from the server.");
@@ -32,14 +34,56 @@ searchButton.addEventListener("click", async (e) => {
   e.preventDefault(); // Prevent the default form submission behavior
 
   const inputValue = movieSearchInput.value; // Get the value from the input field
-
+  iconContainer.style.display = "none";
   // Reset the input field after search
   movieSearchInput.value = "";
-
   // Fetch movie data and handle the response
   const movieData = await fetchApiData(inputValue);
-
   // Check if movieData contains Search results
-  const movieNames = movieData.map((movie) => {});
-  console.log(movieNames);
+
+  displayMovies(movieData);
 });
+
+const displayMovies = (movies) => {
+  movieContainer.innerHTML = "";
+  movies.map((movie) => {
+    fetch(`http://www.omdbapi.com/?apikey=cf0fdf8&i=${movie.imdbID}`)
+      .then((res) => res.json())
+      .then((movie) => {
+        let movieHtml = `
+          <div class="movie-container" id="movie-container">
+            <img
+              src=${movie.Poster ? movie.Poster : ""}
+              alt=""
+              class="movie-poster"
+            />
+            <div class="movie-text-container">
+              <div class="movie-title-container">
+                <p class="movie-title">${movie.Title ? movie.Title : ""}</p>
+                <img src="img/starIcon.png" alt="" srcset="" />
+                <p class="movie-imdb">${
+                  movie.imdbRating ? movie.imdbRating : ""
+                }</p>
+              </div>
+              <div class="movie-info-container">
+                <p class="movie-time">${movie.Runtime ? movie.Runtime : ""}</p>
+                <p class="movie-type">${movie.Genre ? movie.Genre : ""}</p>
+                <div class="watchlist-container">
+                  <img src="img/watchlistIcon.png" alt="" />
+                  <p>Watchlist</p>
+                </div>
+              </div>
+              <div class="movie-explonation">
+                <p>
+                  ${
+                    movie.Plot ? movie.Plot : ""
+                  }<span><a href="">Read More</a></span>
+                </p>
+              </div>
+            </div>
+          </div>
+        `;
+        movieContainer.innerHTML += movieHtml;
+      });
+  });
+};
