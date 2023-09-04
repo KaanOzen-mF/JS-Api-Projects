d852c448b779435bb7df61e51f66addac3b2a6cf
KaanOzen-mF <kaanozn98@gmail.com>
2023-09-04 15:20:08 +0300

    Personal dashboard is done

--- personalDashboard/index.js
+++ personalDashboard/index.js
@@ -1,5 +1,12 @@
 // References element for html
 const imageAuthor = document.getElementById("author");
+const coinContainer = document.getElementById("coin-container");
+const coinPriceContainer = document.getElementById("coin-price-container");
+const coinNameContainer = document.getElementById("coin-name-container");
+const timeContainer = document.getElementById("time-container");
+const weatherContainer = document.getElementById("weather-container");
+const weatherIcon = document.getElementById("weather-icon");
+const weatherInfo = document.getElementById("weather-info");
 
 //Fetch API function, it needs movie name as parameter
 const fetchApiData = async () => {
@@ -25,22 +32,28 @@ const fetchApiData = async () => {
     return null;
   }
 };
-// Function to make the second API call
+
+// Function to make the stock data API call
 const fetchStockData = async () => {
-  const url =
-    "https://twelve-data1.p.rapidapi.com/stocks?exchange=TR&format=json";
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "b12886698dmsh2a39c211cc8b4aep12a465jsn115794388fb5",
-      "X-RapidAPI-Host": "twelve-data1.p.rapidapi.com",
-    },
-  };
+  const url = "https://api.coingecko.com/api/v3/coins/bitcoin";
 
   try {
-    const response = await fetch(url, options);
-    const result = await response.text();
-    console.log(result);
+    const response = await fetch(url);
+    if (!response.ok) {
+      console.log("Something went wrong!");
+    }
+    const result = await response.json();
+
+    coinPriceContainer.innerHTML += `
+    <p>🎯: $${result.market_data.current_price.usd}</p>
+    <p>👆: $${result.market_data.high_24h.usd}</p>
+    <p>👇: $${result.market_data.low_24h.usd}</p>
+    `;
+
+    coinNameContainer.innerHTML += `
+                                  <img src=${result.image.small} > 
+                                  <p>${result.id}</p>                         
+    `;
   } catch (error) {
     console.error(error);
   }
@@ -66,3 +79,48 @@ const fetchStockData = async () => {
     console.log("An error occurred:", error);
   }
 })();
+
+//Update time function
+function updateTime() {
+  const today = new Date();
+  const time = today.toLocaleTimeString("en-us", { timeStyle: "short" });
+
+  timeContainer.innerHTML = `<p>${time}</p>`;
+}
+
+// Initial call to display the time
+updateTime();
+
+// Update the time every minute (60000 milliseconds)
+setInterval(updateTime, 60000);
+
+navigator.geolocation.getCurrentPosition((position) => {
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=b9b153eaf7f306e1eb99f4b89b104917&units=metric`
+  )
+    .then((res) => {
+      if (!res.ok) {
+        throw Error("Weather data not available");
+      }
+      return res.json();
+    })
+
+    .then((data) => {
+      const iconUrl = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+      weatherIcon.innerHTML += `<img src=${iconUrl} >`;
+      weatherInfo.innerHTML += `
+                                      <p>Location: ${data.name}</p>
+                                      <p>Temperature ${Math.floor(
+                                        data.main.temp
+                                      )} °C</p>
+                                      <p>Feels Like: ${Math.floor(
+                                        data.main.feels_like
+                                      )} °C</p>
+                                      <p>${data.weather[0].description}</p>
+                                      <p>${Math.floor(data.wind.speed)} km/h</p>
+                                      `;
+      console.log(data);
+    })
+
+    .catch((err) => console.error(err));
+});
