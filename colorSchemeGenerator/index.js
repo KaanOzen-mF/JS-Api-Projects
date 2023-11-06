126ed864135915aab546c45a400917d4a3d9ea4c
KaanOzen-mF <kaanozn98@gmail.com>
2023-11-06 14:21:47 +0300

    Comment lines added

--- colorSchemeGenerator/index.js
+++ colorSchemeGenerator/index.js
@@ -1,19 +1,24 @@
+// Event listener for the color input field
 document.getElementById("color").addEventListener("change", function () {
   var selectedColor = this.value;
   this.style.backgroundColor = selectedColor; // Set the background color of the select element
 });
 
+// Getting references to various elements in the HTML
 const form = document.getElementById("color-form");
 const colorSelect = document.getElementById("color");
 const modeSelect = document.getElementById("mode");
 const colorPicker = document.getElementById("color-picker-body");
 
+// Event listener for the form submission
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 
+  // Extracting selected color and mode
   let selectedColor = colorSelect.value.replace("#", "");
   const selectedMode = modeSelect.value;
 
+  // Fetching color scheme data from an external API
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${selectedColor}&mode=${selectedMode}&count=6`
   )
@@ -21,14 +26,17 @@ form.addEventListener("submit", function (event) {
     .then((color) => generateColorPalette(color));
 });
 
+// Function to generate color palette
 const generateColorPalette = (color) => {
   colorPicker.innerHTML = "";
   const colorsData = color.colors;
   colorsData.forEach((colorInfo) => {
+    // Creating a container for each color
     const colorContainer = document.createElement("div");
     colorContainer.classList.add("color-container");
     colorContainer.style.backgroundColor = colorInfo.hex.value;
 
+    // Displaying color code
     const colorCode = document.createElement("div");
     colorCode.classList.add("color-code");
     colorCode.textContent = colorInfo.hex.value;
@@ -36,12 +44,15 @@ const generateColorPalette = (color) => {
     colorContainer.appendChild(colorCode);
     colorPicker.appendChild(colorContainer);
 
+    // Adding event listener to copy color code to clipboard
     colorContainer.addEventListener("click", () => {
       copyToClipboard(colorInfo.hex.value);
+      alert(`${colorInfo.hex.value} copy to clipboard`);
     });
   });
 };
 
+// Function to copy text to clipboard
 function copyToClipboard(text) {
   navigator.clipboard
     .writeText(text)
